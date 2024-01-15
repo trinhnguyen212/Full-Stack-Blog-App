aa08e9d030fb10182cd0a71d65e3564bf05d7fd6
trinhnguyen212 <tngu212@mywhitecliffe.com>
2024-01-15 14:39:41 +1300

    first commit

---
+++ client/src/pages/Single.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+
+const Single = () => {
+  return (
+    <div className='single'>
+      <div className="content">
+        <img src="https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+           </div>
+           <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt="" />
+              </Link>
+              <Link>
+                <img src={Delete} alt="" />
+              </Link>
+           </div>
+        </div>
+      
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, sequi!</h1>
+        <p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis saepe, distinctio voluptate harum, unde rerum nulla eveniet ab soluta assumenda, expedita eligendi earum! Libero, alias?</p>
+            <br />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nihil non voluptas dolorum repellendus error at. Commodi temporibus dolorum officiis praesentium voluptatum porro consequuntur, ipsam impedit voluptatem a ea iste saepe error nemo obcaecati, enim amet dolor iusto illo! Nam culpa numquam, laudantium enim atque dolor cumque voluptatem rerum quod facilis modi adipisci aut repellendus doloribus harum qui. Iusto, voluptatem.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis saepe, distinctio voluptate harum, unde rerum nulla eveniet ab soluta assumenda, expedita eligendi earum! Libero, alias?</p>
+            <br />
+            <br />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum nihil non voluptas dolorum repellendus error at. Commodi temporibus dolorum officiis praesentium voluptatum porro consequuntur, ipsam impedit voluptatem a ea iste saepe error nemo obcaecati, enim amet dolor iusto illo! Nam culpa numquam, laudantium enim atque dolor cumque voluptatem rerum quod facilis modi adipisci aut repellendus doloribus harum qui. Iusto, voluptatem.
+            </p>
+        </p>
+      </div>
+      <Menu/>
+    </div>
+  )
+}
+
+export default Single
---
+++ client/src/pages/Write.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Write = () => {
+  return (
+    <div className="write"></div>
+  )
+}
+
+export default Write
