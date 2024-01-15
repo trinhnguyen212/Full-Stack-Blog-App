5efb278860fb7339a21a869bb599238ccc34151d
trinhnguyen212 <tngu212@mywhitecliffe.com>
2024-01-15 17:12:32 +1300

    React-quill Editor

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,17 +1,51 @@
 import React from 'react'
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+  const [value, setValue] = useState('');
+  //console.log(value);
   return (
-    <div className="add">
-      <div className="content">
-        <input type="text" placeholder='Title'/>
+      <div className="add">
+        <div className="content">
+          <input type="text" placeholder='Title'/>
+          <div className="editorContainer">
+            <ReactQuill theme="snow" value={value} onChange={setValue} />
+          </div>
       </div>
       <div className="menu">
         <div className="item">
-          i1
+          <h1>Pulish</h1>
+          <span>
+            <b>Status: </b>Draft
+          </span>
+          <span>
+            <b>Visibility: </b>Public
+          </span>
+          <input style={{display:"none"}} type="file" name="" id="file"/>
+          <label htmlFor="file">Upload Image</label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
         </div>
         <div className="item">
-          i2
+          <h1>Category</h1>
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label htmlFor="art"> ART</label>
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label htmlFor="art"> ART</label>
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label htmlFor="art"> ART</label>
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label htmlFor="art"> ART</label>
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label htmlFor="art"> ART</label>
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label htmlFor="art"> ART</label>
+          <input type="radio" name="cat" value="art" id="art"/>
+          <label htmlFor="art"> ART</label>
         </div>
       </div>
     </div>
