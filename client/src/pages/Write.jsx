3e28759737cb036b38abf99a44f489e1577fb175
trinhnguyen212 <tngu212@mywhitecliffe.com>
2024-01-15 15:38:07 +1300

    Write Page Update

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Write = () => {
   return (
-    <div className="write"></div>
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder='Title'/>
+      </div>
+      <div className="menu">
+        <div className="item">
+          i1
+        </div>
+        <div className="item">
+          i2
+        </div>
+      </div>
+    </div>
   )
 }
 
